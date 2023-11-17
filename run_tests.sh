98ec848d793c2da887e12c110beeb5e6bc8335ee
Cory Mosiman <cory.mosiman12@gmail.com>
2023-11-17 16:11:40 -0500

    chore: this works

---
+++ run_tests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TOOLCHAIN=com.apple.dt.toolchain.XcodeDefault
+IOS_VERSION=${IOS_VERSION:-17.0.1}
+SDK=${SDK:-iphonesimulator17.2}
+BUILD_DIR="$(PWD)/.xcbuild"
+
+SCHEME=SPMPluginXcode
+PROJECT=ExampleXcode/SPMPluginXcode/SPMPluginXcode.xcodeproj
+BUNDLE_PATH="$(PWD)/$SCHEME.xcresult"
+DESTINATION="platform=iOS Simulator,OS=$IOS_VERSION,name=iPhone 12 Pro"
+testArgs=( xcrun --toolchain "$TOOLCHAIN" \
+        xcodebuild build \
+        -toolchain "$TOOLCHAIN" \
+        -project "$PROJECT" \
+        -scheme "$SCHEME" \
+        -configuration Debug \
+        -destination "$DESTINATION"  \
+        -derivedDataPath "$BUILD_DIR" \
+        -resultBundlePath "$BUNDLE_PATH" \
+        -skipPackagePluginValidation \
+        -disableAutomaticPackageResolution \
+        -quiet
+)
+rm -rf "$BUILD_DIR"
+rm -rf "$BUNDLE_PATH"
+echo "Executing build command:"
+echo ""
+echo "${testArgs[@]}"
+echo ""
+
+"${testArgs[@]}"
